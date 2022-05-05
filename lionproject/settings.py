ee0815271c77c1c5299813a499e81dca5abf662f
soljun0402 <soljun0402@gmail.com>
2022-05-06 06:05:20 +0900

    trial1

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-&x3c2ij%k9tf0(l6s8ot43ki%(3sywd51+3@!9+%_d!w4b@k5^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
